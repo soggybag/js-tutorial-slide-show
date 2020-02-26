d2b23b29ecda2f0453f2ed3e33ed9612ba129549
Mitchell <soggybag@gmail.com>
2020-02-25 21:02:35 -0800

    fixed and improved

--- ms-slide-show.js
+++ ms-slide-show.js
@@ -1,98 +1,125 @@
 
 (function() {
-    function makeSlideshow(slides) {
-        // const slides = document.getElementById(slidesId)
-        const slidesInner = slides.querySelector('.slides-inner')
-        const images = slidesInner.querySelectorAll('img')
 
-        // ---------------------------------
-        // Setup buttons 
+  // ---------------------------------------------------------
+  // Define Constants 
 
-        const nextButton = slides.querySelector('.ms-next-button')
-        const prevButton = slides.querySelector('.ms-prev-button')
+  const MS_SLIDES = '.ms-slides'
+  const MS_SLIDES_INNER = '.ms-slides__inner'
+  const MS_SLIDES_NEXT_BUTTON = '.ms-slides__next-button'
+  const MS_SLIDES_PREV_BUTTON = '.ms-slides__prev-button'
+  const MS_SLIDES_INDICATORS = '.ms-slides__indicators'
+  const BUTTON_COLOR = 'rgba(255,255,255,0.5)'
+  const BUTTON_COLOR_SELECTED = 'rgba(255,255,255,1.0)'
 
-        if (nextButton !== null) {
-            nextButton.addEventListener('click', function(e) {
-                e.preventDefault()
-                // clear the interval
-                // add a new interval
-                nextSlide()
-            })
-        }
+  // ---------------------------------------------------------
+  // Make Slides 
 
-        if (prevButton !== null) {
-            prevButton.addEventListener('click', function(e) {
-                e.preventDefault()
+  function makeSlideshow(slides) {
+      // const slides = document.getElementById(slidesId)
+      const slidesInner = slides.querySelector(MS_SLIDES_INNER)
+      const images = slidesInner.querySelectorAll('*')
 
-                prevSlide()
-            })
-        }
+      // ---------------------------------
+      // Setup buttons 
 
-        // ---------------------
-        // Setup indicators 
+      const nextButton = slides.querySelector(MS_SLIDES_NEXT_BUTTON)
+      const prevButton = slides.querySelector(MS_SLIDES_PREV_BUTTON)
 
-        const indicatorContainer = slides.querySelector('.ms-slide-indicators')
-        const indicators = []
-        if (indicatorContainer !== null) {
-            for (let i = 0; i < images.length; i += 1) {
-                const li = document.createElement('li') // <li>
-                indicatorContainer.appendChild(li)
-                indicators.push(li)
-            }
-            indicators[0].style.backgroundColor = 'rgba(255,255,255,1.0)'
-        }
+      console.log(slides)
 
-        // ---------------------
-        // Setup timer 
+      if (nextButton !== null) {
+          nextButton.addEventListener('click', function(e) {
+              e.preventDefault()
+              // clear the interval
+              // add a new interval
+              nextSlide()
+          })
+      }
 
-        const delay = parseInt(slides.dataset.delay)
-        const transition = parseInt(slides.dataset.transition)
-        slidesInner.style.transition = `${transition}ms`
+      if (prevButton !== null) {
+          prevButton.addEventListener('click', function(e) {
+              e.preventDefault()
 
-        const slidesWidth = slides.clientWidth
+              prevSlide()
+          })
+      }
+
+      // ---------------------
+      // Setup indicators 
+
+      const indicatorContainer = slides.querySelector(MS_SLIDES_INDICATORS)
+      const indicators = []
+      if (indicatorContainer !== null) {
+          for (let i = 0; i < images.length; i += 1) {
+              const li = document.createElement('li') // <li>
+              indicatorContainer.appendChild(li)
+              indicators.push(li)
+          }
+          indicators[0].style.backgroundColor = BUTTON_COLOR_SELECTED
+      }
+
+      // ---------------------
+      // Setup timer 
+
+      const delay = parseInt(slides.dataset.delay)
+      const transition = parseInt(slides.dataset.transition)
+
+      slidesInner.style.transition = `${transition}ms`
+
+      const slidesWidth = slides.clientWidth
+      
+      let index = 0
+
+      if (!isNaN(delay) && delay > 0) {
+        setInterval(nextSlide, delay)
+      } 
         
-        let index = 0
+      // clearInterval(interval)
 
-        let interval = setInterval(nextSlide, delay)
-        // clearInterval(interval)
+      // ------------------------
 
-        // ------------------------
+      function nextSlide() {
+          index += 1
+          if (index === images.length) {
+              index = 0
+          }
+          showSlide()
+      }
 
-        function nextSlide() {
-            index += 1
-            if (index === images.length) {
-                index = 0
-            }
-            showSlide()
-        }
+      function prevSlide() {
+          index -= 1
+          if (index < 0) {
+              index = images.length - 1
+          }
+          showSlide()
+      }
 
-        function prevSlide() {
-            index -= 1
-            if (index < 0) {
-                index = images.length - 1
-            }
-            showSlide()
-        }
+      function showSlide() {
+          // CSS - transform : translate3d(0, 0, 0);
+          slidesInner.style.transform = `translate3d(${index * -slidesWidth}px, 0, 0)`
+          // console.log(index * -slidesWidth)
+          indicators.forEach(function(el, i) {
+              if (i === index) {
+                  el.style.backgroundColor = BUTTON_COLOR_SELECTED
+              } else {
+                  el.style.backgroundColor = BUTTON_COLOR
+              }
+          })
+      }
 
-        function showSlide() {
-            // CSS - transform : translate3d(0, 0, 0);
-            slidesInner.style.transform = `translate3d(${index * -slidesWidth}px, 0, 0)`
-            // console.log(index * -slidesWidth)
-            indicators.forEach(function(el, i) {
-                if (i === index) {
-                    el.style.backgroundColor = 'rgba(255,255,255,1.0)'
-                } else {
-                    el.style.backgroundColor = 'rgba(255,255,255,0.5)'
-                }
-            })
-        }
+  } // end makeSlideshow
+  // ------------------------------------------------------------
 
-    } // end makeSlideshow
+  // ------------------------------------------------------------
+  // Initialize Slides 
 
-    const slideshows = document.querySelectorAll('.ms-slide-show')
-    for (let i = 0; i < slideshows.length; i += 1) {
-        makeSlideshow(slideshows[i])
-    }
+  const slideshows = document.querySelectorAll(MS_SLIDES)
+
+  for (let i = 0; i < slideshows.length; i += 1) {
+      makeSlideshow(slideshows[i])
+  }
+
 })() // IIFE
 
 
